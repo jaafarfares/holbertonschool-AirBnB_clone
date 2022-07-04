363ba98b327c71e57938e2aa9418ae26da34e905
jaafarfares <jaafarferes@gmail.com>
2022-07-04 21:00:27 +0100

    file_storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,30 +1,39 @@
 #!/usr/bin/python3
-
+""" class FileStorage that serializes instances to a JSON file
+and deserializes JSON file to instances:"""
 import json
+import os
 
 
 class FileStorage:
+    """
+    """
     __file_path = "file.json"
-    __objects = ""
+    __objects = {}
 
     def all(self):
+        """ returns the dictionary __objects """
         return FileStorage.__objects
 
     def new(self, obj):
+        """sets in __objects the obj with key <obj class name>.id"""
         key = "{}.{}".format(type(obj).__name__, obj.id)
-            FileStorage.__objects[key] = obj
+        FileStorage.__objects[key] = obj
 
     def save(self):
-        filee = FileStorage.__objects
-        objdict = {obj: filee[obj].to_dict() for obj in filee.keys()}
+        """serializes __objects to the JSON file (path: __file_path)"""
+        fstorobj = FileStorage.__objects
+        objdict = {obj: fstorobj[obj].to_dict() for obj in fstorobj.keys()}
         with open(FileStorage.__file_path, "w") as f:
             json.dump(objdict, f)
 
     def reload(self):
+        """Deserializes the JSON file to __objects.
+        (only if the JSON file (__file_path) exists"""
         try:
             with open(self.__file_path, "r", encoding="utf-8") as f:
                 data = json.load(f.read())
                 new_dict = dict()
 
         except Exception:
-            return  
+            return
